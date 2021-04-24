c36c7f5bd3c697dfcd93b85cc391f5d3642a003e
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-04-23 20:45:04 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+famous_person = "guy_buford"
+is_an_astronaut = true
+famous_person = "anthony hopkins"
+num_3 = 7
+num_9 = 10
+sum = num_3 + num_9
+animals = ['lion', 'tiger', 'bear']
+animals.shift
+"famous_person".size
