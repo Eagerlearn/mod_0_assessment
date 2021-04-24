8fafac2b3f6ed016d6d3bda74327920af98b3d80
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-04-23 21:33:28 -0600

    Add school and pets

---
+++ model_b.rb
@@ -0,0 +1,10 @@
+my_pets_growing_up = {
+  "best_dog" => 'champ',
+  "first_dog" => 'cyclops',
+  "years_with_pets" => 18,
+  "total_number_of_dogs" => 5,
+  "taught_tricks" => true
+  "all_big_breeds" => true
+  "all_names" => ['cyclops', 'champ', 'gypsy', 'villian', 'prince'],
+  "all_breeds" => ['golden_retriever', 'labrador_retriever', 'belgian_shepard']
+}
