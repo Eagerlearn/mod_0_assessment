e06fefe92e6524262e9b2bd6e8d05f5a89240588
Craig Cardwell <craigcardwell@Craigs-MacBook-Pro.local>
2021-07-26 14:30:15 -0600

    Update rb models a and b with airport and veh

--- models/model_a.rb
+++ models/model_a.rb
@@ -1,11 +1,13 @@
 # Creating hash with 8 key values (2 strings, 2 integer, 2 boolean, 2 arrays)
-school = {
-  "name" => 'overbrook',
-  "city" => 'philadelphia',
-  "year_built" => 1924,
-  "zip code" => 19131,
-  "is_public_school" => true,
-  "is_still_open" => true,
-  "current_subjects" => ['science', 'math', 'history'],
-  "current_teachers" => ['john_jones', 'jane_jones', 'jonesy_jones']
+airport = {
+  "name" => 'dia',
+  "city" => 'denver',
+  "year_built" => 1995,
+  "zip_code" => 80249,
+  "is_international_airport" => true,
+  "is_largest_land_area" => false,
+  "current_concourses" => ['a', 'b', 'c'],
+  "parking" => ['garage', 'economy', 'shuttle', 'short term', 'pena blvd']
 }
+
+#p airport
--- models/model_b.rb
+++ models/model_b.rb
@@ -1,13 +1,13 @@
 # Creating an hash with 8 key values (2 strings, 2 integers, 2 boolean, 2 arrays)
-favorite_pet = {
-  "name" => 'champ',
+vehicle = {
+  "make" => 'volvo',
   "color" => 'black',
-  "years_lived" => 14,
-  "homes_lived_in" => 1,
-  "learned_tricks" => true,
-  "big_breed" => true,
-  "favorite_activities" => ['running', 'walking', 'sleeping', 'fetch'],
-  "favorite_destinations" => ['chambers_field', 'fairmount_park', 'carroll_park']
+  "years_owned" => 5,
+  "number_of_owners" => 1,
+  "is_awd" => true,
+  "is_six_cyclinder" => false,
+  "options" => ['climate package', 'tech package', 'exterior color', 'drivetrain'],
+  "trims_available" => ['platinum', 'premier', 'r-design']
 }
 
-#p favorite_pet
+#p vehicle
